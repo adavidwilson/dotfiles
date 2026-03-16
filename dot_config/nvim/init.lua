1f78f3a40557bc6ca6c11a01ef291c58df186edb
David Alexander Wilson <david@adavidwilson.com>
2026-03-16 14:53:55 -0400

    add nvim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- TODO:
+--      - Writing mode (margins, no line numbers, other nice writing-focused features)
+
+-- 8-space indents to discourage nesting
+vim.opt.shiftwidth = 8
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.expandtab = true
+
+-- Break lines on words instead of characters
+vim.opt.linebreak = true
